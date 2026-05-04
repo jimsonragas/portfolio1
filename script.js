78cdc59d2b80467d424d9e8aa29cc558677e2453
jimsonragas <jimsond.ragas@gmail.com>
2026-05-04 16:50:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const hero = document.querySelector(".hero");
 
+window.addEventListener("scroll", () => {
+  let scroll = window.scrollY;
+
+  hero.style.transform = `scale(${1 + scroll * 0.0004})`;
+  hero.style.opacity = 1 - scroll * 0.0012;
+});
